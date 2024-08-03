f8e0491966ad7c848150527d4014a2e774a479ba
Tarek Mues <git@tarekmues.com>
2024-08-04 00:31:25 +0200

    refactor webapp to react and add username to messages

--- server.py
+++ server.py
@@ -1,96 +1,16 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from fastapi.websockets import WebSocket, WebSocketState, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from typing import Dict, List
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h4 id="clientId"></h4>
-        <form action="" onsubmit="applyChat(event)" style="margin-bottom: 30px;">
-            <input type="text" id="chatId" autocomplete="off">
-            <button>Apply Chat</button>
-        </form>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            
-            function generateRandomString(length) {
-                const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                let result = '';
-                const charactersLength = characters.length;
-                for (let i = 0; i < length; i++) {
-                    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-                }
-                return result;
-            }
-            
-            const randomString = generateRandomString(10); // Generates a random string of length 10
-            document.getElementById('clientId').innerHTML = randomString;
-            console.log(randomString);
-            
-            var ws;
-            
-            function sendMessage(event) {
-                var input = document.getElementById("messageText");
-                if (ws != null) {
-                    ws.send(input.value);
-                } else {
-                    console.error("Socket is null. Choose chat id before sending messages.");
-                }
-                input.value = '';
-                event.preventDefault();
-            }
-            
-            function applyChat(event) {
-            if (ws != null) {
-               ws.close();
-               ws = null;
-            }
-                var chatId = document.getElementById("chatId")
-                var newSocket = new WebSocket("ws://13.51.64.14:80/chat/" + chatId.value + "/" + randomString);
-                initSocket(newSocket);
-                ws = newSocket;
-                event.preventDefault();
-            }
-            
-            function initSocket(socket) {
-                socket.onmessage = function(event) {
-                    var messages = document.getElementById('messages')
-                    var message = document.createElement('li')
-                    var content = document.createTextNode(event.data)
-                    message.appendChild(content)
-                    messages.appendChild(message)
-                };
-            }
-        </script>
-    </body>
-</html>
-"""
-
-app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="frontend/dist")
+#app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="frontend/dist")
 
 conn_by_chat: Dict[str, List[WebSocket]] = {}
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
-
-
-@app.websocket("/chat/{chat_id}/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, chat_id: str, client_id: str):
+@app.websocket("/chat/{chat_id}/")
+async def websocket_endpoint(websocket: WebSocket, chat_id: str):
     await websocket.accept()
 
     if chat_id in conn_by_chat:
@@ -103,13 +23,13 @@ async def websocket_endpoint(websocket: WebSocket, chat_id: str, client_id: str)
 
             if websocket.client_state != WebSocketState.CONNECTED:
                 print(f"Client State was not connected")
-                connections.remove(websocket)
+                conn_by_chat.get(chat_id).remove(websocket)
                 break
 
             data = await websocket.receive_text()
 
             for i, e in enumerate(conn_by_chat.get(chat_id)):
-                await e.send_text(f"{client_id}: {data}")
+                await e.send_text(f"{data}")
 
     except WebSocketDisconnect as e:
         print(f"Disconnect block triggered")
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,20 +1,109 @@
-import {FC, useState} from 'react';
+import {FC, useCallback, useState} from 'react';
 import './App.css';
+import useWebSocket, {ReadyState} from "react-use-websocket";
 
-export enum ConnectionState {
-    Connected = "Connected",
-    Connecting = "Connecting",
-    NotConnected = "Not Connected"
+export interface ChatMessage extends ApiChatMessage {
+    identifier: string;
+}
+
+export interface ApiChatMessage {
+    clientId: string;
+    message: string;
 }
 
 export const App: FC = () => {
 
-    const [connState, setConnState] = useState<ConnectionState>(ConnectionState.NotConnected);
+    const [socketUrl, setSocketUrl] = useState<string | null>(null);
+    const {sendJsonMessage, readyState} = useWebSocket(socketUrl,
+        {onMessage: (event: WebSocketEventMap['message']) => appendNewMessage(JSON.parse(event.data))});
+
+    const [clientId, setClientId] = useState<string | undefined>(undefined);
+
+    const [chatId, setChatId] = useState<string | undefined>(undefined);
+    const [chatIdForm, setChatIdForm] = useState<string>("");
+    const [userIdForm, setUserIdForm] = useState<string>("");
+
+    const [messageForm, setMessageForm] = useState<string>("");
+
+    const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]);
+
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: 'Connecting',
+        [ReadyState.OPEN]: 'Open',
+        [ReadyState.CLOSING]: 'Closing',
+        [ReadyState.CLOSED]: 'Closed',
+        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    }[readyState];
+
+    function appendNewMessage(content: ApiChatMessage): void {
+        const identifier = generateRandomString(15);
+        setChatHistory([...chatHistory, {identifier, message: content.message, clientId: content.clientId}]);
+    }
+
+    function initSocket(chatIdFormValue: string, userIdFormValue: string): void {
+        if (chatIdFormValue != "" && userIdFormValue != "") {
+            setSocketUrl("ws://16.171.116.146:80/chat/" + chatIdFormValue);
+            setChatId(chatIdFormValue);
+            setClientId(userIdFormValue);
+        } else {
+            console.error("Chat id and client id must not be empty");
+        }
+    }
+
+    const handleClickSendMessage = useCallback((messageContent: string) => {
+        if (readyState == ReadyState.OPEN) {
+            if (messageContent != "") {
+                sendJsonMessage<ApiChatMessage>({clientId, message: messageContent});
+            } else {
+                console.error("Message content must not be empty.");
+            }
+        } else {
+            console.error(`Socket is in state ${connectionStatus} while trying to send messages`);
+        }
+    }, [readyState]);
+
+
+    function generateRandomString(length: number): string {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
 
     return (
         <>
             <h1 className="text-3xl font-semibold">Chat</h1>
-            <h1 className="text-3xl font-semibold">{connState}</h1>
+            <h1 className="text-3xl font-semibold">{chatId != undefined ? chatId : "Chat nicht gesetzt"}</h1>
+            <h1 className="text-3xl font-semibold">{clientId}</h1>
+            <h1 className="text-3xl font-semibold">{connectionStatus}</h1>
+
+            <div className="flex w-full place-content-center">
+                <div className="flex flex-col m-8">
+                    <label>
+                        ChatId: <input value={chatIdForm} onChange={e => setChatIdForm(e.target.value)}
+                               className="input input-bordered" type="text" autoComplete="off"/>
+                    </label>
+                    <label>
+                        UserId: <input value={userIdForm} onChange={e => setUserIdForm(e.target.value)}
+                               className="input input-bordered" type="text" autoComplete="off"/>
+                    </label>
+                    <button className="btn btn-info" onClick={() => initSocket(chatIdForm, userIdForm)}>Set Chat</button>
+                </div>
+                <div className="flex m-8">
+                    <input value={messageForm} onChange={e => setMessageForm(e.target.value)}
+                           className="input input-bordered" type="text" autoComplete="off"/>
+                    <button className="btn btn-info" onClick={() => handleClickSendMessage(messageForm)}>Absenden
+                    </button>
+                </div>
+            </div>
+            <div>
+                <ul>
+                    {chatHistory.map(msg => <li key={msg.identifier}>{msg.clientId}: {msg.message}</li>)}
+                </ul>
+            </div>
         </>
     );
 }
